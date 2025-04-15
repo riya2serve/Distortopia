79a57e751c7435ff64a91ff5ffb5f18887e8b059
Riya Rampalli <riyarampalli@Riyas-MacBook-Pro-7.local>
2025-04-15 09:46:23 -0400

    testing HTML output using pandas

--- distortopia/simparents.py
+++ distortopia/simparents.py
@@ -2,6 +2,7 @@ import os
 import glob
 from Bio import SeqIO
 import argparse
+import pandas as pd #to genrate HTML with color-codes SNPs
 
 """
 This script allows a user to select one genome FASTA from each of two species,
@@ -68,6 +69,43 @@ def compare_contigs(ref_fasta, query_fasta, output_vcf, mode="length", top_n=5):
 
     print(f"\nSNP comparison complete → {output_vcf}")
 
+def gen_HTML(vcf_path, html_out = "snp_summary.hmtl"):
+    """
+    Loads a user-generated VCF file, summarizes SNP per contig, exports a styled HTML table.
+    """ 
+    with open(vcf_path) as f:
+        lines = [line for line in f if not line.startswith("#")]
+
+    df = pd.read_csv(
+        pd.compat.StringIO("".join(lines)),
+        sep="\t", #separating columns 
+        names=["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"] #column names
+    )
+
+    # Filter biallelic SNPs
+    valid = {"A", "C", "G", "T"} #looking for base pairs
+    df = df[(df["REF"].isin(valid)) & (df["ALT"].isin(valid)) & (df["REF"] != df["ALT"])]
+
+    # Count SNPs per contig
+    summary = df["CHROM"].value_counts().reset_index()
+    summary.columns = ["CHROM", "SNP_COUNT"]
+
+    # Color styling
+    def color(val):
+        if val > 50000:
+            return "background-color: #d73027; color: white"
+        elif val > 10000:
+            return "background-color: #fc8d59"
+        elif val > 1000:
+            return "background-color: #fee08b"
+        else:
+            return "background-color: #d9ef8b"
+
+    styled = summary.style.applymap(color, subset=["SNP_COUNT"])
+    styled.to_html(html_out, index=False)
+
+    print(f"SNP summary table written to: {html_out}")
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Compare top contigs from two genomes by length or order")
     parser.add_argument("--ref-dir", required=True, help="Directory containing reference FASTAs")
@@ -86,5 +124,26 @@ if __name__ == "__main__":
     os.makedirs(os.path.dirname(args.out), exist_ok=True)
 
     compare_contigs(ref_fasta, query_fasta, args.out, mode=args.mode, top_n=args.top_n)
+    gen_HTML(args.out, html_out=args.out.replace(".vcf", "_summary.html"))
+
+## ========
+# EXAMPLE INPUT/ OUTPUT 
+## ========
+'''
+User input should look something like this w/ argparse arguments
+'''
+#bash
+##python compare_fasta_snps.py \
+  ###--ref-dir user-data/Arabidopsis_thaliana/ncbi_dataset/data \
+  ###--query-dir user-data/Arabidopsis_lyrata/ncbi_dataset/data \
+  ###--out genomes/athal_vs_alyr_by_order.vcf \
+  ###--mode order \
+  ###--top-n 5
+
+
+
+
+
+
 
 
