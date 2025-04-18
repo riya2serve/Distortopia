8d9630c44b516b8ee66900ff1c0a3e32d72b681e
Riya Rampalli <riyarampalli@Riyas-MacBook-Pro-7.local>
2025-04-18 14:07:54 -0400

    generated a new .paf files using ref genomes with a .gff file available

---
+++ distortopia/manhattan.py
@@ -0,0 +1,11 @@
+
+'''
+This script generates a genome-wide variant density/positional map. 
+It uses the .paf file, and associates SNP positions and contigs. 
+It requires users have: (1) .paf file containing target contig names and (2) SNP positions 
+or approximate locations. These data will allow users to plot each variant along the 
+genomic x-axis, grouped by contig.
+'''
+
+
+
